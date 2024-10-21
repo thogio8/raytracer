95867d1aa9ddd31b212117a0f97cc83e0d5a6130
Thomas Gioana* <thomas.gioana@epitech.eu>
2024-10-21 20:50:13 +0200

    Add some Matrix methods in Point3D

--- Math/Point3D.java
+++ Math/Point3D.java
@@ -25,4 +25,24 @@ public class Point3D{
     public Point3D add(Vector3D v){
         return new Point3D(x + v.x, y + v.y, z + v.z);
     }
+
+    public Matrix toMatrix() {
+        return new Matrix(new double[][]{{x}, {y}, {z}, {1}});
+    }
+
+    public static Point3D fromMatrix(Matrix matrix) {
+        double[][] data = matrix.getData();
+        return new Point3D(data[0][0], data[1][0], data[2][0]);
+    }
+
+    //Main method for testing all the methods
+    public static void main(String[] args){
+        Point3D p1 = new Point3D(1, 2, 3);
+        Point3D p2 = new Point3D(4, 5, 6);
+        Vector3D v = new Vector3D(1, 1, 1);
+        System.out.println(p1.distance(p2));
+        System.out.println(p1.add(v));
+        System.out.println(p1.toMatrix());
+        System.out.println(Point3D.fromMatrix(p1.toMatrix()));
+    }
 }
