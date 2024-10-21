d51790e5297594706fcffdf1471eeb60cc8cd576
Thomas Gioana* <thomas.gioana@epitech.eu>
2024-10-21 20:15:35 +0200

    Create a Ray class

---
+++ RayTracer/Ray.java
@@ -0,0 +1,30 @@
+package RayTracer;
+
+import Math.Point3D;
+import Math.Vector3D;
+
+class Ray{
+    private Point3D origin;
+    private Vector3D direction;
+
+    public Ray(Point3D origin, Vector3D direction){
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+    public Point3D getOrigin(){
+        return origin;
+    }
+
+    public Vector3D getDirection(){
+        return direction;
+    }
+
+    public Point3D getPoint(double t){
+        return origin.add(direction.multiply(t));
+    }
+
+    public String toString(){
+        return "Origin: " + origin + ", Direction: " + direction;
+    }
+}
