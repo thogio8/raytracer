43ca22f3c7604fd7cb867f3caf03afe1e4c12ee4
Thomas Gioana* <thomas.gioana@epitech.eu>
2024-10-23 09:20:31 +0200

    Create Sphere class and implementation of method hit

---
+++ RayTracer/Sphere.java
@@ -0,0 +1,40 @@
+package RayTracer;
+
+import Math.Point3D;
+import Math.Vector3D;
+import Math.Matrix;
+
+class Sphere {
+    private Point3D center;
+    private double radius;
+
+    public Sphere(Point3D center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point3D getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double hit(Ray ray) {
+        Vector3D oc = ray.getOrigin().subtract(center);
+        double a = ray.getDirection().dot(ray.getDirection());
+        double b = 2.0 * oc.dot(ray.getDirection());
+        double c = oc.dot(oc) - radius * radius;
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            return -1.0;
+        } else {
+            return (-b - Math.sqrt(discriminant)) / (2.0 * a);
+        }
+    }
+
+    public String toString() {
+        return "Center: " + center + ", Radius: " + radius;
+    }
+}
